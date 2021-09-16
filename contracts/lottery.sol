71fddca15a2836c3a6e9dcbc3ca660c44e9cb8cc
Devin capriola <devincapriola@gmail.com>
2021-09-16 18:02:12 -0400

    Defining the State Variables and the Constructor

---
+++ contracts/lottery.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Lottery {
+    address payable[] public players;
+    address public manager;
+    
+    constructor() {
+        manager = msg.sender;
+    }
+}
