10e9bccb333a30352a4d1b4233078ece4c4c7310
Devin capriola <devincapriola@gmail.com>
2021-09-18 20:14:11 -0400

    Selecting the Winner and Sending Contract Balance

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -23,4 +23,17 @@ contract Lottery {
     function random() public view returns(uint) {
         return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
     }
+    
+    function pickWinner() public {
+        require(msg.sender == manager);
+        require(players.length >= 3);
+        
+        uint r = random();
+        address payable winner;
+        
+        uint index = r % players.length;
+        winner = players[index];
+        
+        winner.transfer(getBalance());
+    }
 }
